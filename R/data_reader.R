9c23f9f0a507ef8a9f21d34a6a827ce0f9f55936
chowfoon <foon.h.wong@gmail.com>
2019-03-21 16:31:44 -0700

    adding product data/time cap to R reader

--- R/data_reader.R
+++ R/data_reader.R
@@ -1,3 +1,5 @@
+source("R/get_take_rate.R")
+
 data_reader = function(data, data_ref, nrows=Inf, to_factors=TRUE) {
     ref = readr::read_csv(data_ref)
 
@@ -22,5 +24,29 @@ data_reader = function(data, data_ref, nrows=Inf, to_factors=TRUE) {
         parsed = dplyr::mutate_at(parsed, dplyr::vars(fct_col), as.factor)
     }
 
-    parsed
+
+    caps = distinct(parsed, ProductName) %>%
+        mutate_all(.funs = list(CapData_MB = get_datacap, CapTime_Min = get_timecap))
+
+    message("Calculating take rate...")
+
+    parsed %>%
+        left_join(caps) %>%
+        left_join(get_take_rate_df(parsed))
 }
+
+get_datacap = function(nm) {
+    quant = str_extract(nm, regex("([\\d]+) ?MB", ignore_case = TRUE))
+    str_extract(quant, "[\\d]+")
+}
+
+get_timecap = function(nm) {
+    time = str_extract(nm, regex("([\\d]+) ?h|([\\d]+) ?min", ignore_case = TRUE))
+    hours = which(str_detect(time, regex("H", ignore_case = TRUE)))
+
+    time = as.numeric(str_extract(time, "[\\d]+"))
+
+    time[hours] = time[hours] * 60
+    time
+}
+
