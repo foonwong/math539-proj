f183dde8b0e5b65a519b3b51942429adefdcb5e6
chowfoon <foon.h.wong@gmail.com>
2019-03-23 21:57:40 -0700

    notebook

--- python/jupyter_notebooks/test_notebook.py
+++ python/jupyter_notebooks/test_notebook.py
@@ -7,37 +7,32 @@ import re
 import numpy as np
 import pandas as pd
 import wifipricing.data_reader
+pd.set_option('display.max_columns', 999)
+pd.set_option('display.max_rows', 20) 
+
+#%%
 importlib.reload(wifipricing.data_reader)
 from wifipricing.data_reader import data_reader
 
-# #%%
-# wifi_products_df = data_reader(
-#     "data/df_rfp_dataset_raw_20181218185047.csv",
-#     "data/data_reference.csv",
-#     usecols=['ProductName']
-# )
-
-# #%%
-# wifi_products_df.astype(str). \
-#     groupby(['ProductName', 'DataCap_MB', 'TimeCap_min']). \
-#     size()
-
-
-#%%
 wifi_takerate_df = data_reader(
     "data/df_rfp_dataset_raw_20181218185047.csv",
     "data/data_reference.csv",
-    usecols=['FlightID', 'TotalPassengers'],
+    usecols=['FlightID', 'TotalPassengers', 'Category'],
     nrows=5000
 ) 
 
-wifi_takerate_df.head()
+df = wifi_takerate_df[['FlightID', 'TotalPassengers', 'Category']].head(100).\
+    groupby(['FlightID', 'Category']).count().\
+    unstack()
+
+df.columns
+
 
 #%%
-tr_df.head()
+df.head()
 
 #%%
-pd.merge(wifi_takerate_df, tr_df, )
+pd.merge(wifi_takerate_df, tr_df)
 
 
 #%%
@@ -45,5 +40,6 @@ pd.merge(wifi_takerate_df, tr_df, )
 pd.merge(wifi_takerate_df, tr_df, on='FlightID').groupby(['FlightID', 'TakeRate_overall']).size()
 
 #%%
+
 
 #%%
