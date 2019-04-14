7cb1c56ea997fd495d7d202f1b04ae7e9fff1936
chowfoon <spork@bigpotato.localdomain>
2019-04-14 01:02:04 -0700

    data reader calculates per flight datausage/revenue

--- python/jupyter_notebooks/airline_prod.py
+++ python/jupyter_notebooks/airline_prod.py
@@ -1,5 +1,9 @@
 #%%
-sys.path.append('python')
+import os
+# sys.path.append('python')
+
+os.listdir('python')
+os.listdir('python/wifipricing')
 
 #%%
 import importlib
@@ -7,29 +11,29 @@ import re
 import numpy as np
 import pandas as pd
 import wifipricing.data_reader
+from wifipricing.data_reader import data_reader
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 sns.set(color_codes=True)
-e
+
 #%%
 pd.set_option('display.max_columns', 999)
 pd.set_option('display.max_rows', 10) 
 
 #%%
+importlib.reload(wifipricing.data_reader)
+from wifipricing.data_reader import data_reader
+
 colnames_wifi = data_reader(
     "data/df_rfp_dataset_raw_20181218185047.csv",
     "data/data_reference.csv",
     nrows=5
 ).columns
 
-#%%
 colnames_wifi
 
 #%%
-importlib.reload(wifipricing.data_reader)
-from wifipricing.data_reader import data_reader
-
 # df_airline_prod = data_reader(
 #     "data/df_rfp_dataset_raw_20181218185047.csv",
 #     "data/data_reference.csv",
@@ -51,8 +55,11 @@ from wifipricing.data_reader import data_reader
 df_price_cap = data_reader(
     "data/df_rfp_dataset_raw_20181218185047.csv",
     "data/data_reference.csv",
-    usecols=['Price/USD', 'ProductName', 'TotalUsageMB']
-) 
+    usecols=['flight_id', 'routes', 'airline', 'price_usd', 'product_name', 'total_usage_mb']
+)
+
+df_price_cap = df_price_cap.\
+    assign(price_per_mb = lambda x: x['datacap_mb']/ x['price_usd'])
 
 
 #%%
@@ -60,7 +67,7 @@ df_price_cap
 
 #%%
 p_price_data = sns.jointplot(
-    x='Price/USD', y='TotalUsageMB', 
+    x='price_usd', y='total_usage_mb', 
     kind='kde',
     xlim=(0,40),
     ylim=(0,600),
@@ -69,10 +76,165 @@ p_price_data = sns.jointplot(
  )
 
 #%%
-# How many people are using crazy amount of data?
-prop_1gb = round(sum(df_price_cap.TotalUsageMB > 1000) / df_price_cap.shape[0] * 100, 
+prop_1gb = round(sum(df_price_cap.total_usage_mb > 1000) / df_price_cap.shape[0] * 100, 
     ndigits=2)
 
 print(f"Only {prop_1gb}% of customers use more than 1GB of data ")
+
+#%%
+price_levels = df_price_cap.groupby(['price_usd', 'product_name']).size().shape[0]
+data_levels = df_price_cap.groupby(['price_usd', 'datacap_mb']).size().shape[0]
+capped_perc = round(data_levels/price_levels * 100, 2)
+
+not_nulls = df_price_cap.count()['datacap_mb']
+
+capped_ses = round(not_nulls / df_price_cap.shape[0] *100, 2)
+
+print(f'{data_levels} / {price_levels} ({capped_perc}%) of price/product combo have datacap')
+print(f'{not_nulls} / {df_price_cap.shape[0]} ({capped_ses}%) of sessions have datacap')
+
+
+print(f"Only {prop_1gb}% of customers use more than 1GB of data ")
+
+#%% [markdown]
+## Distributions
+
+#%%
+p = sns.distplot(df_price_cap['price_per_mb'].dropna(), )
+p.axes.set_title('Distribution of price per MB')
+
+#%%
+df_price_cap.groupby('airline').size().sort_values().plot(kind='bar')
+
+#%%
+# number of products per airline-route
+p = df_price_cap.groupby(['airline', 'routes', 'product_name']).size().\
+    reset_index().groupby(['airline', 'routes']).size().plot(kind='hist')
+
+p.axes.set_title('Number of products per airline-route (All)')
+
+#%%
+df_price_cap.query('datacap_mb > 0').\
+    groupby(['airline', 'routes', 'product_name']).size().\
+    reset_index().groupby(['airline', 'routes']).size().plot(kind='hist')
+
+p.axes.set_title('Number of products per airline-route (Product with datacap)')
+
+#%%
+df_price_cap.query('timecap_min > 0').\
+    groupby(['airline', 'routes', 'product_name']).size().\
+    reset_index().groupby(['airline', 'routes']).size().plot(kind='hist')
+
+p.axes.set_title('Number of products per airline-route (Product with timecap)')
+
+#%%
+sns.jointplot(
+    x='price_usd', y='datacap_mb', 
+    kind='kde',
+    data=df_price_cap.sample(10000)
+ )
+
+#%%
+sns.jointplot(
+    x='price_usd', y='datacap_mb', 
+    kind='kde',
+    xlim=(0,25),
+    ylim=(0,150),
+    # color='datacap_mb', kind='hex',
+    data=df_price_cap.sample(10000)
+ )
+
+#%%
+p = sns.jointplot(
+    x='price_usd', y='price_per_mb', 
+    kind='kde',
+    data=df_price_cap.sample(10000)
+ )
+
+p.fig.suptitle('Distribution of product pricing vs price per MB')
+
+
+#%%
+p = sns.jointplot(
+    x='price_per_mb', y='total_usage_mb', 
+    kind='kde',
+    xlim=(0,15),
+    ylim=(0,150),
+    data=df_price_cap.sample(10000)
+ )
+
+p.fig.suptitle('Distribution of data usage vs price per MB')
+
+#%%
+p = sns.jointplot(
+    x='datacap_mb', y='total_usage_mb', 
+    kind='kde',
+    xlim=(0,150),
+    ylim=(0,150),
+    data=df_price_cap.sample(10000)
+ )
+
+p.fig.suptitle('Distribution of data usage vs price per MB')
+
+#%%
+p = sns.scatterplot(
+    x=df_price_cap['datacap_mb'], y=df_price_cap['total_usage_mb'],
+    # xlim=(0,150),
+    # ylim=(0,150)
+ )
+
+p.set(xlim=(0,150), ylim=(0,150))
+plt.show()
+
+
+#%% [markdown]
+## Distributions
+### Considering where to limit training data
+
+#%%
+over_50 = df_price_cap.query('price_usd > 50').shape[0]
+over_25 = df_price_cap.query('price_usd > 25').shape[0]
+rows = df_price_cap.shape[0]
+
+print(f'{over_50} / {rows} ({over_50/rows*100}%) of products are over $50 USD') 
+print(f'{over_25} / {rows} ({over_25/rows*100}%) of products are over $25 USD') 
+
+#%%
+df_price_cap.columns
+
+
+#%%
+def get_revenue(df):
+    "Returns overall datausage per FlightID"
+    df_rev = df.groupby('flight_id')['price_usd'].sum().\
+        reset_index().rename(columns={'price_usd':'flight_revenue_usd'})
+
+    df = pd.merge(df, df_rev, on='flight_id', how='left')
+
+    return df
+
+#%%
+def get_flight_data_mb(df):
+    "Returns overall datausage per FlightID"
+    df_rev = df.groupby('flight_id')['price_usd'].sum().\
+        reset_index().rename(columns={'price_usd':'flight_revenue_usd'})
+
+    df = pd.merge(df, df_rev, on='flight_id', how='left')
+
+    return df
+
+#%%
+df_test = data_reader(
+    "data/df_rfp_dataset_raw_20181218185047.csv",
+    "data/data_reference.csv",
+    usecols=['flight_id', 'price_usd', 'total_usage_mb'],
+    nrows=5000
+)
+
+#%%
+# flight_rev = df_price_cap.groupby('flight_id').price_usd.sum()
+
+#%%
+get_revenue(df_test)
 
 #%%
--- python/wifipricing/data_reader.py
+++ python/wifipricing/data_reader.py
@@ -29,16 +29,22 @@ def data_reader(data, data_dict, nrows=None, usecols=None):
         df['timecap_min'] = df['product_name'].map(lambda x: timecap_dict[x])
     
     try:
-        tr_df = get_takerate_overall(df)
-        df = pd.merge(df, tr_df, on=['flight_id', 'total_passengers'], how='left')
+        df_tr = get_takerate_overall(df)
+        df = pd.merge(df, df_tr, on=['flight_id', 'total_passengers'], how='left')
     except:
         pass
 
-    # try:
-    #     tr_df = get_takerate_category(df)
-    #     df = pd.merge(df, tr_df, on=['FlightID', 'TotalPassengers', 'Category'], how='left')
-    # except:
-    #     pass
+    try:
+        df_du = get_flight_data_mb(df)
+        df = pd.merge(df, df_du, on=['flight_id'], how='left')
+    except:
+        pass
+
+    try:
+        df_rev = get_revenue(df)
+        df = pd.merge(df, df_rev, on=['flight_id'], how='left')
+    except:
+        pass
 
     return df 
 
@@ -73,16 +79,31 @@ def get_timecap(x):
 def get_takerate_overall(df):
     """Returns overall wifi takerate per FlightID"""
     tr_df = df.\
-        groupby(['FlightID', 'TotalPassengers']).size().reset_index().rename(columns={0:'TotalSessions'}).\
-        assign(TakeRate_overall=lambda x: x['TotalSessions'] / x['TotalPassengers'])
+        groupby(['flight_id', 'total_passengers']).size().reset_index().rename(columns={0:'totalsessions'}).\
+        assign(flight_takerate = lambda x: x['total_sessions'] / x['total_passengers'])
 
     return tr_df
 
 
-def get_takerate_category(df):
-    """Returns overall wifi takerate per FlightID"""
-    tr_df = df.\
-        groupby(['FlightID', 'TotalPassengers', 'Category']).size().reset_index().rename(columns={0:'TotalSessionsCat'}).\
-        assign(TakeRate_overall=lambda x: x['TotalSessionsCat'] / x['TotalPassengers'])
+def get_revenue(df):
+    "Returns overall revenue per FlightID"
+    df_rev = df.groupby('flight_id')['price_usd'].sum().\
+        reset_index().rename(columns={'price_usd':'flight_revenue_usd'})
 
-    return tr_df
+    return df_rev
+
+#%%
+def get_flight_data_mb(df):
+    """Returns overall datausage per FlightID"""
+    df_du = df.groupby('flight_id')['total_usage_mb'].sum().\
+        reset_index().rename(columns={'total_usage_mb':'flight_data_mb'})
+
+    return df_du
+
+# def get_takerate_category(df):
+#     """Returns overall wifi takerate per FlightID"""
+#     tr_df = df.\
+#         groupby(['FlightID', 'TotalPassengers', 'Category']).size().reset_index().rename(columns={0:'TotalSessionsCat'}).\
+#         assign(TakeRate_overall=lambda x: x['TotalSessionsCat'] / x['TotalPassengers'])
+
+#     return tr_df
