a52610ea500ef3505b03b46a3efbf8779252f503
chowfoon <spork@bigpotato.localdomain>
2019-04-18 10:09:27 -0700

    lgb tuning script

--- python/jupyter_notebooks/demand_model_input.py
+++ python/jupyter_notebooks/demand_model_input.py
@@ -1,19 +1,17 @@
 #%%
 import sys
+import os
 import importlib
 import re
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 sys.path.append('python')
-sns.set(color_codes=True)
 
 
 #%%
 pd.set_option('display.max_columns', 999)
-pd.set_option('display.max_rows', 150) 
+pd.set_option('display.max_rows', 10) 
 
 
 #%%
@@ -24,438 +22,141 @@ InteractiveShell.ast_node_interactivity = "all"
 #%%
 import wifipricing.data_reader
 importlib.reload(wifipricing.data_reader)
+import wifipricing.modeling_prepartions
+importlib.reload(wifipricing.modeling_prepartions)
 import wifipricing.sugar 
 importlib.reload(wifipricing.sugar)
 
 from wifipricing.data_reader import data_reader
-from wifipricing.data_reader import get_product_summary
+from wifipricing.modeling_prepartions import get_splitted_wifi_data 
 from wifipricing.sugar import *
 
-
 #%%
-# Load data
-usecol=[
-    'product_name',
-    'flight_id',
-    'airline',
-    'aircraft_type',
-    'flight_duration_hrs',
-    'total_usage_mb',
-    'seat_count',
-    'total_passengers',
-    'night_flight',
-    'flight_type',
-    'category',
-    'ac_frame',
-    'routes',
-    'price_usd',
-    'ife',
-    'e_xtv',
-    'e_xphone',
-    'one_media',
-    'jack_seat_count',
-    'economy_pct',
-    'bus_pass_percent',
-    'luxury',
-    'origin_iata',
-    'orig_country',
-    'orig_region',
-    # 'destination_iata',
-    'dest_country',
-    'dest_region'
-    # 'airline_region'
-
-    # 'antenna_type',
-    # 'flight_duration_type'
-    # 'session_start_time',
-    # 'session_end_time',
-    # 'session_duration_minutes',
-    # 'session_volume_up_amount',
-    # 'session_volume_down_amount',
-    # 'departure_time_utc',
-    # 'landing_time_utc',
-    # 'departure_time_local',
-    # 'landing_time_local'
-]
-
-df = data_reader(
-    "data/df_rfp_dataset_raw_20181218185047.csv",
-    "data/data_reference.csv",
-    nrows=100000,
-    # skiprows=np.random.randint(1, 4000000, 3500000),
-    usecols=usecol
+df_data, df_time, df_none, df_all = get_splitted_wifi_data(
+    'data/df_rfp_dataset_raw_20181218185047.csv', 
+    'data/data_reference.csv' 
 )
 
-#%%
-preview(df)
-missing_data_report(df)
+df_data.shape
+df_time.shape
+df_none.shape
+df_all.shape
 
 
-#%%
-
-
-# #%%
-# nrow = df.shape[0]
-# datacapped = df[df['datacap']].shape[0]
-# timecapped = df[df['timecap']].shape[0]
-# bothcapped = df[df['datacap'] & df['timecap']].shape[0]
-# nocapped = df[~(df['datacap'] | df['timecap'])].shape[0]
-
-# f'Data: {datacapped} '
-# f'time: {timecapped} '
-# f'both: {bothcapped} '
-# f'None: {nocapped} '
-
-
-# #%%
-# df['pass_seat_ratio'] = df['total_passengers'] / df['seat_count']
-# df['pass_jack_ratio'] = df['total_passengers'] / df['jack_seat_count']
-
-# df[df['pass_seat_ratio'] > 1][['aircraft_type', 'total_passengers', 'seat_count']].\
-#     pipe(preview)
-
-
-
-# #%%
-# df_summarized_datacap = df[df['datacap']].pipe(get_product_summary)
-
-# #%%
-# df_summarized_timecap = df[df['timecap']].pipe(get_product_summary)
-
-# nocapcol = [x for x in df.columns if ('cap' not in x) and ('price_per' not in x)]
-# df_summarized_nocap = df[~(df['datacap'] | df['timecap'])][nocapcol].\
-#     pipe(get_product_summary)
-
-
-
-
-# #%%
-# df_summarized_datacap.shape[0]
-# df_summarized_timecap.shape[0]
-# df_summarized_nocap.shape[0]
-
-# #%%
-# df_summarized_datacap.head(5)
-# df_summarized_timecap.head(5)
-# df_summarized_nocap.head(5)
-
-# #%%
-# df.groupby(['flight_id', 'product_name']).size().sort_values(ascending=False)
-
-# #%%
-# p = sns.distplot(df_summarized_datacap['profit_per_psn'])
-
-# #%%
-# p = sns.distplot(df_summarized_timecap['profit_per_psn'])
-
-# #%%
-# p = sns.distplot(df_summarized_nocap['profit_per_psn'])
-
-
-# #%%
-# sns.scatterplot(
-#     x='price_usd', hue='datacap_mb',
-#     y='profit_per_psn',
-#     data=df_summarized_datacap.sample(5000)
-# )
-
-# #%%
-# # Prepare for model: datacapped data
-# mdlcols=[
-#     # created columns
-#     'datacap_mb'
-#     , 'has_timecap'
-#     , 'profit_per_psn'
-
-#     # Original features
-#     , 'airline'
-#     , 'aircraft_type'
-#     , 'flight_duration_hrs'
-#     , 'seat_count'
-#     , 'night_flight'
-#     , 'flight_type'
-#     , 'category'
-#     , 'ac_frame'
-#     # , 'routes'
-#     , 'price_usd'
-#     , 'ife'
-#     , 'e_xtv'
-#     , 'e_xphone'
-#     , 'one_media'
-#     , 'jack_seat_count'
-#     , 'economy_pct'
-#     , 'bus_pass_percent'
-#     , 'luxury'
-
-#     # , 'origin_iata'
-#     , 'orig_country'
-#     # , 'orig_region'
-#     # , 'destination_iata'
-#     , 'dest_country'
-#     # , 'dest_region'
-#     # , 'airline_region'
-
-#     # , 'antenna_type'
-#     # , 'flight_duration_type'
-#     # , 'session_start_time'
-#     # , 'session_end_time'
-#     # , 'session_duration_minutes'
-#     # , 'session_volume_up_amount'
-#     # , 'session_volume_down_amount'
-#     # , 'departure_time_utc'
-#     # , 'landing_time_utc'
-#     # , 'departure_time_local'
-#     # , 'landing_time_local'
-# ]
-
-# f'Quick columns check: \
-#     {[x for x in mdlcols if (x not in df_summarized_datacap.columns)]}\
-#     is not in summary dataframe.'
-
-# #%%
-# df_model = df_summarized_datacap.\
-#     assign(has_timecap = lambda x: x['price_per_min'].notnull()).\
-#     loc[:, mdlcols].\
-#     pipe(pd.get_dummies).\
-#     pipe(move_target_to_end, target='profit_per_psn')
-
-# df_model.head()
-# f'model dataframe dimensions: {df_model.shape}'
-
-# #%%
-# from sklearn.preprocessing import StandardScaler, OneHotEncoder
-
-# # col_encod = [c for c, t in zip(df_model.columns, df_model.dtypes) if t = 'category']
-# # # col_scale = df_model.columns
-# # col_encod
-
-# df_model.columns
-# df_model.dtypes
-
-
-# #%%
-# x = df_model.iloc[:, :-1]
-# y = df_model.iloc[:, -1]
-
-# x
-# y
-
-
-
-# #%%
-# from sklearn.model_selection import KFold
-# kfold = KFold(n_splits =3)
-
-# for train_ind, test_ind in kfold.split(x):
-#     print("\nFOLD")
-#     print("train", train_ind)
-#     print("test", test_ind)
-
-
-# #%%
-# x.iloc[train_ind, :]
-# y[train_ind]
-
-
-# #%%
-# # LightGBM 
-# from lightgbm import LGBMRegressor
-# lgb_reg = LGBMRegressor(silent=False)
-
-# lgb_reg.fit(x.iloc[train_ind, :], y[train_ind])
-
-# prediction = lgb_reg.predict(x.iloc[test_ind, :])
-
-
-# #%%
-# feat_imp = lgb_reg.feature_importances_
-
-# pd.DataFrame({'feat':x.columns, 'imp':feat_imp}).\
-#     sort_values('imp', ascending=False).\
-#     iloc[:20, :].\
-#     plot(kind='bar', x='feat')
-
-
-# #%%
-# smp = np.random.randint(1, 90000, size = 10000)
-
-# y_test = np.array(y[test_ind])
-
-# sns.jointplot(prediction[smp], y_test[smp], kind='kde',
-#     ylim = (0,1), xlim=(0,1))
-
-
-# #%%
-# # Good o random forest
-# from sklearn.ensemble import RandomForestRegressor
-# rf_reg = RandomForestRegressor()
-
-# rf_reg.fit(x.iloc[train_ind, :], y[train_ind])
-
-# prediction_rf = rf_reg.predict(x.iloc[test_ind, :])
-
-
-# #%%
-# feat_imp_rf = rf_reg.feature_importances_
-
-# pd.DataFrame({'feat':x.columns, 'imp':feat_imp_rf}).\
-#     sort_values('imp', ascending=False).\
-#     iloc[:20, :].\
-#     plot(kind='bar', x='feat')
-
-
-# #%%
-# smp = np.random.randint(1, 90000, size = 10000)
-
-# y_test = np.array(y[test_ind])
-
-# sns.jointplot(prediction_rf[smp], y_test[smp], kind='kde',
-#     ylim = (0,1), xlim=(0,1))
-
-
-# #%%
-# #  Lasso
-# from sklearn.linear_model import Lasso
-# lasso_reg = Lasso(alpha=0.06)
-
-# lasso_reg.fit(x.iloc[train_ind, :], y[train_ind])
-
-# prediction_las = lasso_reg.predict(x.iloc[test_ind, :])
-
-
-# #%%
-# lasso_reg.coef_
-
-# pd.DataFrame({'feat':x.columns, 'imp':lasso_reg.coef_}).\
-#     sort_values('imp', ascending=False).\
-#     iloc[:20, :].\
-#     plot(kind='bar', x='feat')
-
-
-# #%%
-# smp = np.random.randint(1, 90000, size = 10000)
-
-# y_test = np.array(y[test_ind])
-
-# sns.jointplot(prediction_rf[smp], y_test[smp], kind='kde',
-#     ylim = (0,1), xlim=(0,1))
-
-
-# #%%
-# # from sklearn import linear_model
-# # from sklearn.model_selection import GridSearchCV
-
-# # hyper_p = {'alpha': np.logspace(-6, 2, 10)}
-# # print(hyper_p)
-
-# # lasso = linear_model.Lasso(random_state=1337)
-# # lassoCV = GridSearchCV(lasso, hyper_p)
-
-# # lassoCV.fit(x, y)
-
-
-# #%%
-# # lassoCV.cv_results_
-
-# # scores = lassoCV.cv_results_['mean_test_score']
-# # scores_std = lassoCV.cv_results_['std_test_score']
-
-# # plt.semilogx(hyper_p['alpha'], scores)
-
-# # #%%
-
-# # prediction = lasso.predict(x.iloc[test_ind, :])
-# # [(pred, y) for pred, y in zip(prediction, y[test_ind])]
-
-# # p = sns.scatterplot(prediction, y[test_ind])
-# # p.set(xlabel='prediction', ylabel='data', xlim=(-1,1), ylim=(-1,1))
-
-
-# # #%%
-# # import re
-# # # lassoCV.cv_results_[f'split{x}_test_score' for x in range(3alpha_)]
-# # split_scores = [lassoCV.cv_results_[y] for y in lassoCV.cv_results_ if re.match('split.+test_score', y)]
-
-# # plt.semilogx(hyper_p['alpha'], split_scores[1])
-
-# # #%%
-# # # Corr plot
-# # cols = ['price_per_mb',
-# #  'price_per_min',
-# #  'data_per_psn',
-# #  'revenue_per_psn',
-# #  'profit_per_psn',
-# #  'datacap_mb'] + list(['night_flight'])
-
-# # #%%
-# # g = sns.PairGrid(df_summarized_datacap[cols].sample(1000), hue='night_flight')
-# # g.map_lower(sns.kdeplot)
-# # g.map_upper(sns.scatterplot)
-# # g.map_diag(sns.kdeplot)
-
-
-# # #%%
-# # sns.distplot(prod_summary['profit_per_psn'])
-
-# # #%%
-# # prod_summary[['routes', 'profit_per_psn']].sort_values('profit_per_psn', ascending=False)
-
-# # #%%
-# # distinct(prod_summary, ['orig_country', 'orig_region'])
-
-# # #%%
-# # # prod_summary.groupby('routes').agg({'mean':'profit_per_psn'})
-# # prod_summary.groupby('orig_country')['profit_per_psn'].mean().reset_index().\
-# #     sort_values('profit_per_psn', ascending=False)
-# #     # query('orig_country.str.contains("Fin")')
-
-# # #%%
-# # prod_summary.groupby('dest_country')['profit_per_psn'].mean().reset_index().\
-# #     sort_values('profit_per_psn', ascending=False)
-# #     # query('orig_country.str.contains("Fin")')
-
-# # # prod_summary.groupby('dest_country')['profit_per_psn'].mean().reset_index().\
-# # #     sort_values('profit_per_psn', ascending=False)
-
-# # # sns.boxplot(x='routes', y='profit_per_psn', 
-# # #     data=prod_summary)
-
-
-# # #%%
-# # prod_summary.shape
-
-# #%%
-# from sklearn.preprocessing import StandardScaler, OneHotEncoder
-
-# # col_encod = [c for c, t in zip(df_model.columns, df_model.dtypes) if t = 'category']
-# # # col_scale = df_model.columns
-# # col_encod
-
-# # cats = get_categorical_columns(df_model) 
-# #%%
-
-
-
-# #%%
-# df_model.one_media.dtype
-
-# #%%
-# testbool = data_reader('data/df_rfp_dataset_raw_20181218185047.csv',
-#     data_dict='data/data_reference.csv',
-#     nrows=100,
-#     usecols=['ife', 'e_xtv', 'e_xphone', 'one_media'])
-
-# preview(testbool)
-
-# #%%
-# testbool = pd.read_csv('data/df_rfp_dataset_raw_20181218185047.csv',
-#     # data_dict='data/data_reference.csv',
-#     nrows=50000, sep=',',
-#     usecols=['IFE', 'eXTV', 'eXPhone', 'OneMedia'],
-#     dtype={'IFE':'bool', 'e_xtv':'bool', 'e_xphone':'bool', 'one_media':'bool'})
-
-# preview(testbool)
 
 #%%
+from sklearn.preprocessing import StandardScaler
+
+def get_lgb_data(df_summarized):
+    # These columns are created during summary, but not used for modeling
+    drop_cols = ['product_name', 'flight_id', 'data_per_psn', 'revenue_per_psn', 'total_passengers', 'profit_per_psn']
+
+    X = df_summarized.drop(columns=drop_cols)
+    y = df_summarized['profit_per_psn']
+
+    cats = X.select_dtypes(['category']).columns
+    nums = X.select_dtypes(['float64']).columns
+
+    cat_ref = dict()
+    print(f'\n* The {len(cats)} convert categorical to label encoding for lgb:') 
+    for c in cats:
+        print(c)
+        cat_ref[c] = X[c].cat.categories
+    
+    X[cats] = X[cats].apply(lambda x: x.cat.codes)
+
+    print(f'\n* The {len(nums)} numerical columns will be centered and scaled:') 
+    for c in nums:
+        print(c)
+
+    scaler = StandardScaler()
+    X[nums] = scaler.fit_transform(X[nums])
+
+    return {'data':X, 'target':y, 'labels':cat_ref, 'categorical':cats}
+
+lgb_data = get_lgb_data(df_data)
+
+lgb_data['data']
+lgb_data['target']
+lgb_data['categorical']
+
 
 #%%
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from lightgbm import LGBMRegressor
+
+X_train, X_test, y_train, y_test = train_test_split(lgb_data['data'], 
+                                                    lgb_data['target'],
+                                                    test_size=0.2)
+
+# Tuning lgb:
+seed = 1337
+
+params = {
+    'boosting_type': 'gbdt',
+    'objective': 'binary',
+    'nthread': 3, # Updated from nthread
+    'learning_rate': 0.05,
+    'subsample_for_bin': 200,
+    'subsample': 1,
+    'subsample_freq': 1,
+    'colsample_bytree': 0.8,
+    'reg_alpha': 5,
+    'reg_lambda': 10,
+    'min_split_gain': 0.5,
+    'min_child_weight': 1,
+    'min_child_samples': 5,
+    'scale_pos_weight': 1,
+    'num_class' : 1,
+    'metric' : 'binary_error'
+}
+
+gridParams = {
+    'learning_rate': [0.005],
+    'n_estimators': [40],
+    'max_depth': [3, 5, 7],
+    'num_leaves': [60, 70, 80],
+    'min_data_in_leaf': [500, 1000, 1500],
+    'random_state' : [seed], 
+    'bagging_fraction' : np.logspace(-1, 0, 3),
+    'feature_fraction' : np.logspace(-1, 0, 3), 
+    'lambda_l2' : np.logspace(-3, 2, 10) 
+}
+
+lgb_reg = LGBMRegressor(task='train',
+    boosting_type= 'gbdt',
+    n_jobs = 4, 
+    subsample_for_bin = params['subsample_for_bin'],
+    subsample = params['subsample'],
+    subsample_freq = params['subsample_freq'],
+    min_split_gain = params['min_split_gain'],
+    min_child_weight = params['min_child_weight'],
+    min_child_samples = params['min_child_samples'],
+    scale_pos_weight = params['scale_pos_weight'],
+    device='gpu'
+)
+
+lgb_grid = GridSearchCV(lgb_reg, gridParams,
+    verbose=15,
+    cv=4, n_jobs=3
+)
+
+lgb_grid
+
+#%%
+lgb_grid.fit(X_train, y_train)
+
+
+#%%
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+print(f'MAE train: {mean_absolute_error(y_train, lgb_grid.predict(X_train))}')
+print(f'MSE train: {mean_squared_error(y_train, lgb_grid.predict(X_train))}')
+
+print(f'\nMAE validate: {mean_absolute_error(y_test, lgb_grid.predict(X_test))}')
+print(f'MSE validate: {mean_squared_error(y_test, lgb_grid.predict(X_test))} ')
+
+#%%
+from sklearn.externals import joblib
+joblib.dump(lgb_grid, 'lgb_grid_dummy.pkl')
